fed63691c8b6b1b0d445f34167d175d20e67a273
Angel <acarrascocesa@gmail.com>
2025-07-28 11:07:31 -0400

    actualizacion 28 julio 2

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.title("🏟️ Voluntarios Juegos Centroamericanos y del Caribe 2026")
 # Cargar datos (WPForms)
 ###############################################################################
 
-FILE = "wpforms-45824-Formulario-de-Voluntarios-2025-07-28-11-25-01.csv"
+FILE = "wpforms-45824-Formulario-de-Voluntarios-2025-07-28-15-06-40.csv"
 
 @st.cache_data(show_spinner="Cargando datos…")
 def load_data(path: str) -> pd.DataFrame:
