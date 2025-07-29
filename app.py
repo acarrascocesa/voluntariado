3a155321005c5ece066ad8c973f7ac25c6e3a52c
Angel <acarrascocesa@gmail.com>
2025-07-29 11:42:30 -0400

    actualizacion 28 julio 5

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.title("🏟️ Voluntarios Juegos Centroamericanos y del Caribe 2026")
 # Cargar datos (WPForms)
 ###############################################################################
 
-FILE = "wpforms-45824-Formulario-de-Voluntarios-2025-07-29-13-01-32.csv"
+FILE = "wpforms-45824-Formulario-de-Voluntarios-2025-07-29-15-37-18.csv"
 
 
 @st.cache_data(show_spinner="Cargando datos…")
